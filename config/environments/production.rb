a29261df6228acdc91b49350465b3c9dbc7fa5fa
borzoni <borzoni@mail.ru>
2016-08-19 15:06:19 +0300

    sendgrid setup, screw gmail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -52,12 +52,13 @@ Rails.application.configure do
   
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
+    address:              'smtp.sendgrid.net',
     port:                 587,
-    domain:               'mail.google.com',
-    user_name:            'orderclocks@gmail.com',
-    password:             ENV['SMTP_PASSWORD'],
-    authentication:        :plain
+    domain:               'heroku.com',
+    user_name:            ENV['SENDGRID_USERNAME'],
+    password:             ENV['SENDGRID_PASSWORD'],
+    authentication:        :plain,
+    enable_starttls_auto: true
   }
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = { host: 'http://enigmatic-mountain-84656.herokuapp.com', port: $PORT }
